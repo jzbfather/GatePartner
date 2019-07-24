2a4973ac6eeb467c124279503972c65f2f8b4ffc
vivienjzb <vivienzjb@gmail.com>
2019-07-24 16:28:51 +0100

    Update sms management coding

--- object_appz.sql
+++ object_appz.sql
@@ -1,35 +1,70 @@
-
-CREATE or REPLACE PROCEDURE  GATEPARTNER_SCHEDULE 
+create or replace PROCEDURE POSTMAN 
 AS 
 
 r INT :=0;
 t INT := 0;
-count_sms INT;
-count_mail INT;
-server_address VARCHAR2(255);
-server_port NUMBER;
+
+func VARCHAR2(50);
+plsql_block VARCHAR2(500);
+
+
+config_list intranet.jzb_common_utility.TypeTabArray;
+
+CURSOR ini_param IS select cnf_name,cnf_value from config;
 
 BEGIN
 
-------------------------------------------------------------------------------
------------------------------ Send SMS ---------------------------------------
-------------------------------------------------------------------------------
+  -- ************************************** --
+  -- initialize parameter                   --
+  -- ************************************** --
+  
+  FOR modinfo IN ini_param LOOP      
+      config_list( modinfo.cnf_name ) := modinfo.cnf_value ;
+    
+  END LOOP;
 
-DBMS_OUTPUT.PUT_LINE( 'Valeur de r est 1. Fin de fonction ');
+
+  -- ************************************** --
+  -- SMS management                         --
+  -- ************************************** --
+  
+
+  FOR pu1 IN ( SELECT id, receiver, msg, operator FROM gatepartner.inbox WHERE msgtype = 'SMS'
+             AND status= 'send' AND ROWNUM <= config_list('sms-batch') )
+
+  LOOP
+
+        func := pu1.operator;
+        
+        plsql_block := 'BEGIN  :v := ' || func || '(:v1, :v2); END;';
+        
+        EXECUTE IMMEDIATE plsql_block USING OUT r, IN pu1.receiver, pu1.msg;
+        
+        
+		-- response good or bad
+        IF r = 1 THEN
+
+			UPDATE gatepartner.inbox SET status = 'sent', senttime= TO_CHAR ( SYSDATE, 'DD/MM/YYYY HH24:MI:SS' )
+			WHERE id = pu1.id;
+			COMMIT;
+		ELSE 
+
+			UPDATE gatepartner.inbox SET status = 'notsent', senttime= TO_CHAR ( SYSDATE, 'DD/MM/YYYY HH24:MI:SS' )
+			WHERE id = pu1.id;
+			COMMIT;            
+            
+		END IF;
+
+
+  END LOOP;
 
 
 
-
-
-
-------------------------------------------------------------------------------
------------------------------ Send E-MAIL ------------------------------------
-------------------------------------------------------------------------------
-
+  -- ************************************** --
+  -- E-mail management                      --
+  -- ************************************** --
 
 
 
 
 END;
-
-
---
+++ object_common_fct.sql
@@ -0,0 +1,45 @@
+create or replace PACKAGE jzb_common_utility IS 
+  TYPE TypeTabArray IS TABLE OF VARCHAR2(3200) INDEX BY VARCHAR2(50);
+  
+  -- Convert timestand to date 
+  FUNCTION convert_ts_to_date( p_ts IN NUMBER )
+    RETURN  DATE;
+  
+  FUNCTION convert_date_to_ts( PDate in date )
+    RETURN  NUMBER;
+  
+  PROCEDURE PrintTabArray (nmtab TypeTabArray);
+  
+END;
+/
+
+create or replace PACKAGE BODY jzb_common_utility IS 
+  
+  -- Convert timestand to date 
+  FUNCTION convert_ts_to_date( p_ts IN NUMBER ) RETURN DATE IS
+      l_date DATE;
+  BEGIN
+      l_date := date '1970-01-01' + p_ts/60/60/24;
+      RETURN l_date;
+  END;
+  
+  FUNCTION convert_date_to_ts( PDate in date ) RETURN number is    
+    l_unix_ts number;
+    
+  BEGIN
+    
+       l_unix_ts := ( PDate - date '1970-01-01' ) * 60 * 60 * 24;
+       return l_unix_ts;
+  END;
+  
+  PROCEDURE PrintTabArray (nmtab TypeTabArray) IS
+    
+  BEGIN
+    
+    FOR i IN 1..nmtab.count LOOP
+      dbms_output.put_line(nmtab(i));
+    END LOOP;
+  END;
+  
+  
+END;
